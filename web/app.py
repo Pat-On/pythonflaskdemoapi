55e57a0b59b0e63a69de1658545fb07f2711c328
Patryk <patryk.r.nowak@gmail.com>
2021-07-18 13:32:53 +0100

    API CHART

--- web/app.py
+++ web/app.py
@@ -4,4 +4,14 @@ each user gets 10 tokens
 store a sentence on out database for 1 token
 retrieve his stored sentence for 1 token
 
+API chart
+Resource             |    Address      | Protocol  |   Param                       |  Response + Status
+Register user        |    /register    | Post      | username password string      |  200 ok
+store sentence       |  /store         | post      | username password sentence    |  200ok
+                                                                                      301 no token
+                                                                                      302 inv P & Un
+retrieve sentence    | /get            | GET       | user n + p                    |  200 ok
+                                                                                      301 no token
+                                                                                      302 inv P and UN
+
 """
