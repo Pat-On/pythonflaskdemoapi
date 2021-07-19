e9eec2dfa574e65bdfd4fe2f37d279dbc8d9e1b8
Patryk <patryk.r.nowak@gmail.com>
2021-07-19 13:01:23 +0100

    prep vol1

--- imageClassification/app.py
+++ imageClassification/app.py
@@ -37,11 +37,43 @@ users = db["Users"]
 
 
 # helper function
-def UserExist(username):
+def user_exist(username):
     if users.find({"Username": username}).count() == 0:
         return False
     else:
         return True
+
+
+def verify_pw(username, password):
+    if not user_exist(username):
+        return False
+    hashed_pw = users.find({
+        "Username": username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode("utf8"), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+
+def verify_credentials(username, password):
+    if not user_exist(username):
+        return generated_return_dictionary(301, "Invalid Username"), True  # two things to return
+
+    correct_pw = verify_pw(username, password)
+    if not correct_pw:
+        return generated_return_dictionary(302, "Invalid Password"), True
+
+    return None, False
+
+
+def generated_return_dictionary(status, msg):
+    ret = {
+        "status": status,
+        "msg": msg
+    }
+    return ret
 
 
 class Register(Resource):
@@ -50,7 +82,7 @@ class Register(Resource):
         username = posted_data["username"]
         password = posted_data["password"]
 
-        if UserExist(username):
+        if user_exist(username):
             ret_json = {
                 "status": 301,
                 "msg": "invalid username"
@@ -69,3 +101,46 @@ class Register(Resource):
         }
 
         return jsonify(ret_json)
+
+
+class Classify(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        username = posted_data["username"]
+        password = posted_data["password"]
+        url = posted_data["url"]
+
+        # TODO:
+        ret_json, error = verify_credentials(username, password)
+
+        if error:
+            return jsonify(ret_json)
+
+        tokens = users.find({
+            "Username": username
+        })[0]["Tokens"]
+
+        # TODO
+        if tokens <= 0:
+            return jsonify(generated_return_dictionary(303, "Not enough tokens"))
+
+        r = requests.get(url)
+        ret_json = {}
+
+        with open("temp.jpg", "wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen("python classify_image.py --model_dir=. --image_file=./temp.jpg")
+            proc.communicate()[0]
+            proc.wait()
+            with open("text.txt") as g:
+                ret_json = json.load(g)
+
+        users.update({
+            "Username": username
+        }, {
+            "$set": {
+                "Tokens": tokens - 1
+            }
+        })
+        return ret_json
